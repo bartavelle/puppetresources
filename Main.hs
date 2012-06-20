42a54eda5b017a4b0c25f0acfbd8f8b21df2947e
bartavelle <bartavelle@gmail.com>
2012-06-20 12:00:42 +0200

    Add support for printing templates.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import System.Environment
 import Puppet.Init
 import Puppet.Daemon
 import Puppet.Printers
+import Puppet.Interpreter.Types
 import Facter
 import Data.List
 import System.IO
@@ -11,8 +12,8 @@ usage = error "Usage: puppetresource puppetdir nodename"
 
 main = do
     args <- getArgs
-    let (puppetdir, nodename) | (length args /= 2) = usage
-                                | otherwise = (args !! 0, args !! 1)
+    let (puppetdir, nodename) | (length args /= 2) && (length args /= 3) = usage
+                              | otherwise = (args !! 0, args !! 1)
         prefs = genPrefs puppetdir
     queryfunc <- initDaemon prefs
     rawfacts <- allFacts
@@ -24,4 +25,11 @@ main = do
     resp <- queryfunc nodename allfacts
     case resp of
         Left err -> hPutStrLn stderr err
-        Right x -> putStrLn $ showFCatalog x
+        Right x -> if length args == 3
+            then case (Map.lookup ("file",args !! 2) x) of
+                Nothing -> error "File not found"
+                Just r  -> case (Map.lookup "content" (rrparams r)) of
+                    Nothing -> error "This file has no content"
+                    Just (ResolvedString c)  -> putStrLn c
+                    Just x -> print x
+            else putStrLn $ showFCatalog x
