710b8f6c93e796d4f3d527e78b85e129f1b79772
bartavelle <bartavelle@gmail.com>
2013-04-10 13:56:00 +0200

    updated debian building system

--- mkdeb.sh
+++ mkdeb.sh
@@ -1,5 +1,15 @@
+VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+    echo "please set version"
+    exit 4
+fi
+
 cp equivs dist/build/puppetresources/ && \
+    cp ../language-puppet/ruby/calcerb.rb dist/build/puppetresources/ && \
     cd dist/build/puppetresources && \
+    sed -i -e "s/Version: .*/Version: $VERSION/" equivs && \
     equivs-build -f equivs && \
     mv puppetresources_*_amd64.deb ../../../ && \
     rm -f puppetresources*.changes && \
